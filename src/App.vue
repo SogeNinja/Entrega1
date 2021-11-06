48a66871f97e2c4853d51f83127918363326e73e
mrGoonies <93497614+mrGoonies@users.noreply.github.com>
2021-11-06 01:06:29 -0300

    Configurando App para notificacion push

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,35 @@
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+/* eslint-disable no-unused-vars */
+import { initializeApp } from 'firebase/app';
+import {  getMessaging, getToken, onMessage} from 'firebase/messaging';
+
+
+// Agregamos la app a Firebase
+const firebaseConfig = initializeApp({
+  apiKey: "AIzaSyBGllO5X7krg6T-yVrE6bWI_jBACEA3L98",
+  authDomain: "ionic-project-b7cfe.firebaseapp.com",
+  projectId: "ionic-project-b7cfe",
+  storageBucket: "ionic-project-b7cfe.appspot.com",
+  messagingSenderId: "954743611901",
+  appId: "1:954743611901:web:fc5fe6c30a3a2122a2b994"
+});
+
+// Configurando credenciales por medio de token
+const messaging = getMessaging(); 
+getToken(messaging, { vapidKey: 'BB72NBzQmltzySVHWCiWS_iZNSy8UHdr7qQIwTJ5yYaNLY9BDSb3mpeIgjrXfzjwr-zidRN13KPruyCcZoFszSE' }).then((currentToken) => {
+  if (currentToken) {
+    console.log(currentToken);
+  } else {
+    console.log('No existe un token disponible');
+
+  }
+});
+
+// Push notification config
+
+
 
 export default defineComponent({
   name: 'App',
@@ -15,5 +44,11 @@ export default defineComponent({
     IonRouterOutlet
   }
 });
+
+
+/* eslint-disable no-unused-vars */
+
+
+
 
 </script>
